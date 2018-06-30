5fbc09036e0c35288d5ff97e64d95d46c65da6be
西谷祐樹 <the.heavy-works@outlook.jp>
2018-06-30 13:10:33 +0900

    Initial Commit

---
+++ XcodeTask1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeTask1
+//
+//  Created by 西谷祐樹 on 2018/06/30.
+//  Copyright © 2018年 西谷祐樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
